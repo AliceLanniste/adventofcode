7e76b8859f21a1615f1cc93238f962ec35b2b636
Alice <1399789151@qq.com>
2019-05-23 17:44:25 +0800

    completed day9

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,95 +1,153 @@
-use std::fs;
+use std::fmt;
+use std::io::{self, Write};
 
-// 466 players; last marble is worth 71436 points
+type Result<T> = std::result::Result<T, Box<std::error::Error>>;
 
-fn main() {
-    
-
+fn main() -> Result<()> {
+    part1()?;
+    part2()?;
+    Ok(())
 }
 
+fn part1() -> Result<()> {
+    const PLAYERS: usize = 466;
+    const LAST_MARBLE: u32 = 71736;
 
-#[derive(Debug)]
+    let mut players = vec![Player::default(); PLAYERS];
+    play_game(&mut players, &mut Circle::new(), LAST_MARBLE);
+    writeln!(
+        io::stdout(),
+        "winning score (part 1): {}",
+        players.iter().map(|p| p.points).max().unwrap(),
+    )?;
+    Ok(())
+}
+
+fn part2() -> Result<()> {
+    const PLAYERS: usize = 466;
+    const LAST_MARBLE: u32 = 7173600;
+
+    let mut players = vec![Player::default(); PLAYERS];
+    play_game(&mut players, &mut Circle::new(), LAST_MARBLE);
+    writeln!(
+        io::stdout(),
+        "winning score (part 2): {}",
+        players.iter().map(|p| p.points).max().unwrap(),
+    )?;
+    Ok(())
+}
+
+fn play_game(players: &mut [Player], circle: &mut Circle, marbles: u32) {
+    let start = circle.max_marble_value() + 1; // 1 for fresh game
+    let end = start + marbles;
+    for (player_id, value) in (0..players.len()).cycle().zip(start..end) {
+        circle.turn(&mut players[player_id], value);
+    }
+}
+
+#[derive(Clone, Debug, Default)]
 struct Player {
     points: u32,
 }
 
 
-#[derive(Debug)]
 struct Circle {
     marbles: Vec<Marble>,
-    current: usize,
+    current: MarbleID,
 }
+
+type MarbleID = usize;
+type MarbleValue = u32;
 
 #[derive(Debug)]
 struct Marble {
-    value: u32,
-    prev: usize,
-    next:  usize,
+    value: MarbleValue,
+    prev: MarbleID,
+    next: MarbleID,
 }
 
 impl Circle {
     fn new() -> Circle {
-        let first = Marble {value: 0, prev: 0,next:0 };
-
-        Circle {marbles: vec![first], current: 0}
+        let first = Marble { value: 0, prev: 0, next: 0 };
+        Circle { marbles: vec![first], current: 0 }
     }
 
-    fn play(&mut self, player: &mut Player, value: u32)  {
+    fn turn(&mut self, player: &mut Player, value: MarbleValue) {
         let marble_id = self.add_marble(value);
         if value % 23 != 0 {
-            let insert = self.clockwise(1);
+            let insert_at = self.clockwise(1);
             self.insert_after(marble_id, insert_at);
             self.current = marble_id;
+            return;
         }
+
+        player.points += value;
+        let remove_id = self.counter_clockwise(7);
+        player.points += self.marbles[remove_id].value;
+        self.remove(remove_id);
+        self.current = self.counter_clockwise(6);
     }
 
-    fn add_marble(&mut self, value: u32)  {
+    fn max_marble_value(&self) -> MarbleValue {
+        (self.marbles.len() - 1) as MarbleValue
+    }
+
+    fn add_marble(&mut self, value: MarbleValue) -> MarbleID {
         let id = self.marbles.len();
-        self.marbles.push(Marble::new(value));
+        self.marbles.push(Marble::unlinked(value));
         id
     }
 
-    fn insert_after(&mut self, to: usize,after:usize)  {
-        let temp = self.marbles[after].next;
-        self.marbles[after].next = to;
-        self.marbles[temp].prev = to;
-        self.marbles[to].prev = after;
-        self.marbles[to].next = temp;
-        
+    fn insert_after(&mut self, to_insert: MarbleID, after: MarbleID) {
+        let old_next = self.marbles[after].next;
+        self.marbles[after].next = to_insert;
+        self.marbles[old_next].prev = to_insert;
+        self.marbles[to_insert].prev = after;
+        self.marbles[to_insert].next = old_next;
     }
 
-    fn remove(&mut self,id: usize)  {
-        let(prev, next) = (self.marbles[id].prev, self.marbles[id].next);
-        self.marbles[id].prev = next;
-        self.marbles[id].next = prev;
+    fn remove(&mut self, id: MarbleID) {
+        let (prev, next) = (self.marbles[id].prev, self.marbles[id].next);
+        self.marbles[prev].next = next;
+        self.marbles[next].prev = prev;
     }
 
-
-    fn clockwise(&mut self, mut i: usize) -> usize {
+    fn clockwise(&mut self, mut i: usize) -> MarbleID {
         let mut id = self.current;
         while i > 0 {
             id = self.marbles[id].next;
-            i- = 1;
+            i -= 1;
         }
-
         id
     }
 
-
-    fn counter_clockwise(&mut self, mut i: usize) -> usize {
-        let mut id = self.count;
-        while  i > 0 {
+    fn counter_clockwise(&mut self, mut i: usize) -> MarbleID {
+        let mut id = self.current;
+        while i > 0 {
             id = self.marbles[id].prev;
-            i- = 1;
+            i -= 1;
         }
-
         id
     }
 }
 
 impl Marble {
+    fn unlinked(value: MarbleValue) -> Marble {
+        Marble { value, prev: 0, next: 0 }
+    }
+}
 
-    fn new() -> Self {6
-        Marble {value , prev: 0, next: 0}
+impl fmt::Debug for Circle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut id = self.current;
+        loop {
+            let m = &self.marbles[id];
+            write!(f, "{} ", m.value)?;
+            id = m.next;
+            if id == self.current {
+                break;
+            }
+        }
+        Ok(())
     }
 }
