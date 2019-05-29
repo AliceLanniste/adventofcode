87064ba2028acf4b4f14e756eddcc0a7cb7a3a67
Alice <1399789151@qq.com>
2019-05-29 11:13:32 +0800

    add README

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -3,16 +3,16 @@ use std::fs;
 use std::result;
 use std:: io::{self, Write};
 
-macro_rules! err {
-    ($($tt:tt)*) => { Err(Box::<Error>::from(format!($($tt)*))) }
-}
+// macro_rules! err {
+//     ($($tt:tt)*) => { Err(Box::<Error>::from(format!($($tt)*))) }
+// }
 
 type Result<T> = result::Result<T, Box<Error>>;
 
 
 fn main() -> Result<()> {
     
-     let mut input = fs::read_to_string("data/text.txt").unwrap();
+     let mut input = fs::read_to_string("data/day8.txt").unwrap();
 
     let mut flat = vec![];
     for number in input.split_whitespace() {
@@ -21,6 +21,8 @@ fn main() -> Result<()> {
     let tree = Node::build_tree(&flat);
 
     part1(&tree)?;
+    part2(&tree)?;
+
     Ok(())
 }
 
@@ -30,11 +32,16 @@ fn part1(root: &Node) -> Result<()> {
  Ok(())
 }
 
+fn part2(root: &Node) -> Result<()> {
+     writeln!(io::stdout(), "{}", root.sum_value())?;
+    Ok(())
+}
+
 
 #[derive(Debug)]
 struct Node {
     metadata: Vec<i32>,
-    childern:     Vec<Node>,
+    children:     Vec<Node>,
     len :  usize,
 
 }
@@ -44,13 +51,13 @@ impl Node {
     // 根据数据流来构造一棵树
     fn build_tree( stream: &[i32]) -> Node {
         let(child_num, meta_num) = (stream[0],stream[1]);
-        let mut node = Node {childern: Vec::new(),metadata: Vec::new(),len: 2};
+        let mut node = Node {children: Vec::new(),metadata: Vec::new(),len: 2};
 
-    // 首先构造顶端节点 A (metadata:Vec<i32> , childeren: Vec<node>)
+    // 首先构造顶端节点 A (metadata:Vec<i32> , children: Vec<node>)
         for _ in 0..child_num {
             let child = Node:: build_tree(&stream[node.len..]);
             node.len +=child.len;
-            node.childern.push(child);
+            node.children.push(child);
         } 
 
         // 2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
@@ -70,8 +77,24 @@ impl Node {
 
     fn sum_metadata(&self) -> i32 {
         let mut sum = self.metadata.iter().cloned().sum();
-        for child in &self.childern {
+        for child in &self.children {
             sum += child.sum_metadata();
+        }
+        sum
+    }
+
+     fn sum_value(&self) -> i32 {
+        if self.children.is_empty() {
+            return self.metadata.iter().cloned().sum::<i32>();
+        }
+
+        let mut sum = 0;
+        for &i in &self.metadata {
+            let child = match self.children.get(i as usize - 1) {
+                None => continue,
+                Some(child) => child,
+            };
+            sum += child.sum_value();
         }
         sum
     }
