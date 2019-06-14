88a48ab27d0a1bdf167cfcde5f08d609f3d3c6ba
Alice <1399789151@qq.com>
2019-06-14 16:08:48 +0800

    completed day10

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -4,32 +4,43 @@ extern crate regex;
 
 use regex::Regex;
 use std::fs;
-use std::str::FromStr;
 use std::error::Error;
 use std::cmp;
+use std::io::{self, Write};
+use std::str::{self, FromStr};
 
 type Result<T> = std::result::Result<T,Box<Error>>;
 
-//组成单词的面积是最小的，在之后面积会变大，因此要想获得单词，成立的条件是new_area > current_area,
+//组成单词的面积是最小的，在之后面积会变大，因此要想获得最小的面积，
+// 成立的条件是new_area > current_area,因此循环条件应为
+//while new_area< current_area,
+// 如果数据过大，width*height会导致数据溢出，无论是i32,i64还是usize
+// 因此使用width+height取代width*height，可以运行正常
 
-fn main() {
-    let mut input = fs::read_to_string("data/day10.txt").unwrap();
+
+fn main() ->Result<()>{
+    let mut input = fs::read_to_string("data/text.txt").unwrap();
     let mut Points_vec: Vec<Point> = vec![];
     for line in input.lines() {
         let point = line.parse().expect("error");
         Points_vec.push(point);
     }
-    let points_struct = Points::new(Points_vec);
-    
+    let mut points_struct = Points::new(Points_vec);
+    let mut  current_area = std::i32::MAX;
+    let mut  new_area =  points_struct.area();
 
-    loop {
-// 必须加上current points排列
-        
+    while new_area < current_area {
+        points_struct.turning(true);
+        current_area = new_area;
+        new_area = points_struct.area();
     }
-
+   
+    points_struct.turning(false);
+    writeln!(io::stdout(),"{}",points_struct.second)?;
+    writeln!(io::stdout(), "{}", points_struct.make_string().trim())?;
+    Ok(())
 }
-// 对比此时和下时刻的面积，当下时刻面积大于此时的面积就是组成字母
-// 首先得把min和max确定
+
 
 #[derive(Debug,Clone)]
 struct Points {
@@ -43,13 +54,24 @@ impl Points {
         Points {points, second: 0}
     }
 
-    fn turing(&mut self)  {
-        for p in &mut self.points {
-            p.x +=p.vx;
-            p.y +=p.vy;
+    fn turning(&mut self,cond:bool)  {
+        if cond {
+            for p in &mut self.points {
+                p.x +=p.vx;
+                p.y +=p.vy;
+            }
+            self.second += 1;
+            }
+        else {
+            for p in &mut self.points {
+                p.x -=p.vx;
+                p.y -=p.vy;
+            }
+            self.second -= 1;
+            }
+
         }
-        self.second += 1;
-    }
+    
 
     fn bound(&self) -> Bound {
         
@@ -69,12 +91,37 @@ impl Points {
         Bound {minX, maxX, minY, maxY}
     }
 
-    fn area(&self) -> u32{
+    fn area(&self) -> i32{
         let bound =self.bound();
         let width = bound.width();
         let height = bound.height();
 
-        width * height
+        width + height
+    }
+
+    fn width_height(&self) -> (usize, usize) {
+        let bound =self.bound();
+        
+        (bound.width() as usize, bound.height() as usize)
+    }
+
+
+    fn make_string(&self) -> String {
+        let (width,height) = self.width_height();
+        let bounds = self.bound();
+        let mut grid = vec![vec![b'.'; width]; height];
+        for p in &self.points {
+            let x = bounds.normal_x(p.x);
+            let y = bounds.normal_y(p.y);
+            grid[y as usize][x as usize] = b'#';
+        }
+
+        let mut buf = String::new();
+        for row in grid {
+            buf.push_str(str::from_utf8(&row).unwrap());
+            buf.push('\n');
+        }
+        buf
     }
 }
 
@@ -89,12 +136,28 @@ struct Bound {
 
 impl Bound {
 
-    fn width(&self) -> u32 {
-        (self.maxX - self.minX + 1) as u32 
+    fn normal_x(&self, x: i32) -> u32 {
+        if self.minX >= 0 {
+            (x - self.minX) as u32
+        } else {
+            (x + self.minX.abs()) as u32
+        }
     }
 
-    fn height(&self) -> u32 {
-        (self.maxY - self.minY + 1) as u32
+    fn normal_y(&self, y: i32) -> u32 {
+        if self.minY >= 0 {
+            (y - self.minY) as u32
+        } else {
+            (y + self.minY.abs()) as u32
+        }
+    }
+
+    fn width(&self) -> i32 {
+        (self.maxX - self.minX + 1) 
+    }
+
+    fn height(&self) -> i32 {
+        (self.maxY - self.minY + 1) 
     }
 }
 
@@ -130,6 +193,12 @@ impl FromStr for Point {
         })
     }
 }
+
+
+
+
+    
+       
 
 
 
