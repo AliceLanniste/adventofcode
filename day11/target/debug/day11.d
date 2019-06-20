b91b3939710a2f7b3c9215b983a2a7680a57104f
Alice <1399789151@qq.com>
2019-06-20 17:47:28 +0800

    ./target cleaned

--- day11/target/debug/day11.d
+++
@@ -1,1 +0,0 @@
-D:\adventofcode\day11\target\debug\day11.pdb: D:\adventofcode\day11\src\main.rs
--- day11/target/debug/deps/day11-a4a1ea40d23a8e79.d
+++
@@ -1,5 +0,0 @@
-D:\adventofcode\day11\target\debug\deps\day11-a4a1ea40d23a8e79.exe: src\main.rs
-
-D:\adventofcode\day11\target\debug\deps\day11-a4a1ea40d23a8e79.d: src\main.rs
-
-src\main.rs:
